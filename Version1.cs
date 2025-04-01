af8fa8b46a2c2451db540301ed161fdd1a4677c6
Bhavana <BhavanaaR@saglobal.com>
2025-04-01 15:29:30 +0530

    Update Version1.cs
    Added the chrome driver

--- Version1.cs
+++ Version1.cs
@@ -1,1 +1,17 @@
-Created a New Feature 
+
+// New Feature for logic
+using OpenQA.Selenium;
+
+namespace PracticeTest.TestSuites
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public static IWebDriver driver;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // add the google 
+            // need to add thelogin part logic 
+              IWebDriver driver = new ChromeDriver();
+
